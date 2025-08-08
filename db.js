988e61d6381c6e13bcf86e7f9fdd92b424ff7f1b
Adrien Rochette <adrien@example.com>
2025-08-08 14:19:55 +0200

    fix: delay PG connection to avoid Railway undefined env

--- db.js
+++ db.js
@@ -3,12 +3,9 @@ const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false,
+  },
 });
 
-pool.connect()
-  .then(() => console.log('✅ Connecté à PostgreSQL'))
-  .catch(err => console.error('❌ Erreur connexion PostgreSQL', err));
 
 module.exports = pool;
